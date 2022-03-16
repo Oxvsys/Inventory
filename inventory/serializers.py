0d079d0bdaad389397a35ed4ae2a3da4012e6a9f
PAWAR-AKSHAY <pawar.pawar1@gmail.com>
2022-03-16 12:49:00 +0530

    v0.0.2 - Adds bulk create and update product api

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,8 +1,38 @@
+from django.db import IntegrityError
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from inventory import models
+
+
+class ProductBulkCreateUpdateSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        product_data = [models.Product(**item) for item in validated_data]
+        return models.Product.objects.bulk_create(product_data)
+
+    def update(self, instance, validated_data):
+        instance_hash = {index: i for index, i in enumerate(instance)}
+        result = [
+            self.child.update(instance_hash[index], attrs)
+            for index, attrs in enumerate(validated_data)
+        ]
+        writable_fields = [
+            x
+            for x in self.child.Meta.fields
+            if x not in self.child.Meta.read_only_fields
+        ]
+
+        try:
+            self.child.Meta.model.objects.bulk_update(result, writable_fields)
+        except IntegrityError as e:
+            raise ValidationError(e)
+
+        return result
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
         fields = ["id", "title", "description", "price", "type", "visible", "discount", "created_at", "updated_at"]
+        read_only_fields = ['id', ]
+        list_serializer_class = ProductBulkCreateUpdateSerializer
--- inventory/views.py
+++ inventory/views.py
@@ -1,18 +1,56 @@
-from rest_framework import generics, status
+from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 from inventory import models, serializers
 
 
+def validate_ids(data, field="id", unique=True):
+
+    if isinstance(data, list):
+        id_list = [int(x[field]) for x in data]
+
+        if unique and len(id_list) != len(set(id_list)):
+            raise ValidationError("Multiple updates to a single {} found".format(field))
+
+        return id_list
+
+    return [data]
+
+
 class ProductView(generics.ListCreateAPIView):
-    queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, many=True)
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get("data", {}), list):
+            kwargs["many"] = True
+        return super(ProductView, self).get_serializer(*args, **kwargs)
+
+    def get_queryset(self, ids=None):
+        if ids :
+            queryset = models.Product.objects.filter(id__in=ids)
+        else:
+            queryset = models.Product.objects.all()
+        return queryset
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+
+        ids = validate_ids(request.data)
+
+        instances = self.get_queryset(ids=ids)
+
+        serializer = self.get_serializer(
+            instances, data=request.data, partial=False, many=True
+        )
+
         serializer.is_valid(raise_exception=True)
-        try:
-            self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
